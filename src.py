f2b218cebb904f91dc1032e578123c3d5e9f5cd8
Benjamin Bossan <benjamin.bossan@gmail.com>
2023-01-24 17:38:53 +0100

    Initial working version

---
+++ src.py
@@ -0,0 +1,129 @@
+import operator
+import re
+from contextlib import contextmanager
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as _get_version
+from typing import Callable, Dict
+
+from packaging.version import InvalidVersion, Version, parse
+
+
+__all__ = ["versiondispatch"]
+
+
+_OP_TABLE = {
+    "==": operator.eq,
+    ">=": operator.ge,
+    "<=": operator.le,
+    ">": operator.gt,
+    "<": operator.lt,
+}
+_OP_PAT = re.compile(r"|".join(_OP_TABLE))
+BinOp = Callable[[Version, Version], bool]
+
+
+def _split_package_version(package_version: str) -> tuple[str, str, BinOp]:
+    match = _OP_PAT.search(package_version)
+    if match is None:
+        raise ValueError("Version not correctly specified, should be like 'foo<=1.2.3'")
+
+    package, version = _OP_PAT.split(package_version)
+    op = _OP_TABLE[match.group()]
+    return package.strip(), version.strip(), op
+
+
+def _is_valid_version(version: str) -> bool:
+    valid = True
+    try:
+        Version(version)
+    except InvalidVersion:
+        valid = False
+    return valid
+
+
+def _is_valid_package(package: str) -> bool:
+    valid = True
+    try:
+        _get_version(package)
+    except PackageNotFoundError:
+        valid = False
+    return valid
+
+
+def get_version(package: str) -> Version:
+    return Version(_get_version(package))
+
+
+@contextmanager
+def pretend_version(version_dict: Dict[str, str]):
+    """TODO"""
+    global get_version
+
+    get_version_orig = get_version
+
+    def get_version(package: str):
+        return Version(version_dict.get(package, _get_version(package)))
+
+    yield
+
+    get_version = get_version_orig
+
+
+def _matches_version(package: str, version: str, op: BinOp) -> bool:
+    v0 = get_version(package)
+    v1 = parse(version)
+    return op(v0, v1)
+
+
+def _matches_all_versions(package_versions: list[tuple[str, str, BinOp]]) -> bool:
+    all_match = all(
+        _matches_version(package, version, op)
+        for package, version, op in package_versions
+    )
+    return all_match
+
+
+class versiondispatch:
+    """TODO"""
+    def __init__(self, func):
+        self._func = func
+        self._func._is_versiondispatched = True
+
+        self._funcname = getattr(self._func, '__name__', 'versiondispatch function')
+        self._impl = self._func  # use initial func by default
+        self._matched_version = ""
+
+    def register(self, package_versions: str):
+        splits = [pv.strip() for pv in package_versions.replace(";", ",").split(",")]
+        return self._register(splits)
+
+    def _register(self, package_versions: list[str]):
+        packages_versions = []
+        for package_version in package_versions:
+            package, version, operator = _split_package_version(package_version)
+
+            if not (_is_valid_package(package) and _is_valid_version(version)):
+                raise ValueError(f"{self._funcname} uses incorrect version spec: {package_version}")
+
+            packages_versions.append((package, version, operator))
+
+        def outer(func):
+            if getattr(func, "_is_versiondispatched", False) is True:
+                raise ValueError(
+                    "You are nesting versiondispatch, which is not supported, instead provide "
+                    "multiple version, e.g. '@versiondispatch(\"foo=1.0\", \"bar=2.0\")'"
+                )
+
+            self._impl._is_versiondispatched = True
+
+            if _matches_all_versions(packages_versions):
+                self._impl = func
+                self._matched_version = version
+
+            return self._impl
+
+        outer._is_versiondispatched = True
+        return outer
+
+    def __call__(self, *args, **kwargs):
+        return self._impl(*args, **kwargs)
---
+++ test.py
@@ -0,0 +1,252 @@
+import pytest
+from importlib.metadata import version as _get_version
+
+from src import versiondispatch, pretend_version
+
+# sanity checks
+major = int(_get_version("rich").split(".", 1)[0])
+assert 1 <= major < 1000
+major = int(_get_version("pytest").split(".", 1)[0])
+assert 1 <= major < 1000
+
+
+class TestOneCheck:
+    # check a single package
+
+    def get_func(self):
+        @versiondispatch
+        def func(bar, baz="baz"):
+            return f"default {bar}-{baz}"
+
+        @func.register("rich<1.0")
+        def _old(bar, baz="baz"):
+            return f"old {bar}-{baz}"
+
+        @func.register("rich>=1000")
+        def _new(bar, baz="baz"):
+            return f"new {bar}-{baz}"
+
+        @func.register("rich==1.2.3")
+        def _exact(bar, baz="baz"):
+            return f"exact {bar}-{baz}"
+
+        return func
+
+    def test_no_match(self):
+        func = self.get_func()
+        assert func("hi", baz="there") == "default hi-there"
+
+    def test_lt(self):
+        with pretend_version({"rich": "0.1"}):
+            func = self.get_func()
+            assert func("hi", baz="there") == "old hi-there"
+
+    def test_gt(self):
+        with pretend_version({"rich": "1001.0.0"}):
+            func = self.get_func()
+            assert func("hi", baz="there") == "new hi-there"
+
+    def test_exact(self):
+        with pretend_version({"rich": "1.2.3"}):
+            func = self.get_func()
+            assert func("hi", baz="there") == "exact hi-there"
+
+
+class TestMultipleChecks:
+    # check two package versions
+
+    def get_func(self):
+        @versiondispatch
+        def func(bar, baz="baz"):
+            return f"default default {bar}-{baz}"
+
+        @func.register("rich<1.0, pytest<=1")
+        def _old_old(bar, baz="baz"):
+            return f"old old {bar}-{baz}"
+
+        @func.register("rich<1.0;pytest>1234.5.6")
+        def _old_new(bar, baz="baz"):
+            return f"old new {bar}-{baz}"
+
+        @func.register("rich>=1000 , pytest <= 1")
+        def _new_old(bar, baz="baz"):
+            return f"new old {bar}-{baz}"
+
+        @func.register("rich>=1000.0 ;pytest>1234.5.6")
+        def _new_new(bar, baz="baz"):
+            return f"new new {bar}-{baz}"
+
+        return func
+
+    def test_no_match(self):
+        func = self.get_func()
+        assert func("hi", baz="there") == "default default hi-there"
+
+    def test_only_first_matches(self):
+        with pretend_version({"rich": "0.1", "pytest": "3.2.1"}):
+            func = self.get_func()
+            assert func("hi", baz="there") == "default default hi-there"
+
+    def test_only_second_matches(self):
+        with pretend_version({"rich": "3.2.1", "pytest": "0.0.1"}):
+            func = self.get_func()
+            assert func("hi", baz="there") == "default default hi-there"
+
+    def test_both_match_lt_lt(self):
+        with pretend_version({"rich": "0.1", "pytest": "0.0.1"}):
+            func = self.get_func()
+            assert func("hi", baz="there") == "old old hi-there"
+
+    def test_both_match_lt_gt(self):
+        with pretend_version({"rich": "0.1", "pytest": "9999.99.99dev"}):
+            func = self.get_func()
+            assert func("hi", baz="there") == "old new hi-there"
+
+    def test_both_match_gt_lt(self):
+        with pretend_version({"rich": "5555", "pytest": "0.0.1"}):
+            func = self.get_func()
+            assert func("hi", baz="there") == "new old hi-there"
+
+    def test_both_match_gt_gt(self):
+        with pretend_version({"rich": "5555", "pytest": "9999.99.99dev"}):
+            @versiondispatch
+            def func(bar, baz="baz"):
+                return f"default default {bar}-{baz}"
+
+            @func.register("rich<1.0, pytest<=1")
+            def _old_old(bar, baz="baz"):
+                return f"old old {bar}-{baz}"
+
+            @func.register("rich<1.0;pytest>1234.5.6")
+            def _old_new(bar, baz="baz"):
+                return f"old new {bar}-{baz}"
+
+            @func.register("rich>=1000 , pytest <= 1")
+            def _new_old(bar, baz="baz"):
+                return f"new old {bar}-{baz}"
+
+            @func.register("rich>=1000.0 ;pytest>1234.5.6")
+            def _new_new(bar, baz="baz"):
+                return f"new new {bar}-{baz}"
+
+            assert func("hi", baz="there") == "new new hi-there"
+
+    def test_both_match_exact_exact(self):
+        with pretend_version({"rich": "1.2.3", "pytest": "3.2.1"}):
+            @versiondispatch
+            def func(bar, baz="baz"):
+                return f"default default {bar}-{baz}"
+
+            @func.register("rich<1.0, pytest<=1")
+            def _old_old(bar, baz="baz"):
+                return f"old old {bar}-{baz}"
+
+            @func.register("rich<1.0;pytest>1234.5.6")
+            def _old_new(bar, baz="baz"):
+                return f"old new {bar}-{baz}"
+
+            @func.register("rich>=1000 , pytest <= 1")
+            def _new_old(bar, baz="baz"):
+                return f"new old {bar}-{baz}"
+
+            @func.register("rich>=1000.0 ;pytest>1234.5.6")
+            def _new_new(bar, baz="baz"):
+                return f"new new {bar}-{baz}"
+
+            @func.register("rich==1.2.3 ;pytest==3.2.1")
+            def _exact_exact(bar, baz="baz"):
+                return f"exact exact {bar}-{baz}"
+
+            assert func("hi", baz="there") == "exact exact hi-there"
+
+
+class TestMixedMultipleChecks:
+    # partially check one or two package versions
+
+    def get_func(self):
+        @versiondispatch
+        def func(bar, baz="baz"):
+            return f"default default {bar}-{baz}"
+
+        @func.register("rich<1.0")
+        def _old_rich(bar, baz="baz"):
+            return f"old rich {bar}-{baz}"
+
+        @func.register("pytest<1.0")
+        def _old_pytest(bar, baz="baz"):
+            return f"old pytest {bar}-{baz}"
+
+        @func.register("rich<1.0, pytest<1.0")
+        def _old_old(bar, baz="baz"):
+            return f"old old {bar}-{baz}"
+
+        return func
+
+    def test_no_match(self):
+        func = self.get_func()
+        assert func("hi", baz="there") == "default default hi-there"
+
+    def test_old_rich_match(self):
+        with pretend_version({"rich": "0.1.2"}):
+            func = self.get_func()
+            assert func("hi", baz="there") == "old rich hi-there"
+
+    def test_old_pytest_match(self):
+        with pretend_version({"pytest": "0.1.2"}):
+            func = self.get_func()
+            assert func("hi", baz="there") == "old pytest hi-there"
+
+    def test_both_match(self):
+        with pretend_version({"rich": "0.1.2", "pytest": "0.1.2"}):
+            func = self.get_func()
+            assert func("hi", baz="there") == "old old hi-there"
+
+
+def test_nested_versiondispatch_raises():
+    match = "You are nesting versiondispatch, which is not supported"
+
+    @versiondispatch
+    def func():
+        return "func"
+
+    with pytest.raises(ValueError, match=match):
+        @func.register("rich<1.0")
+        @func.register("pytest<=1")
+        def _():
+            return "bar"
+
+
+def test_invalid_package_version_spec():
+    @versiondispatch
+    def func():
+        return "foo"
+
+    match = "Version not correctly specified, should be like"
+    with pytest.raises(ValueError, match=match):
+        @func.register("rich=1.0")
+        def _(bar, baz="baz"):
+            return "bar"
+
+
+def test_invalid_version_spec():
+    @versiondispatch
+    def func():
+        return "foo"
+
+    match = "func uses incorrect version spec: rich==1.foo.0"
+    with pytest.raises(ValueError, match=match):
+        @func.register("rich==1.foo.0")
+        def _(bar, baz="baz"):
+            return "bar"
+
+
+def test_invalid_package():
+    @versiondispatch
+    def func():
+        return "foo"
+
+    match = "func uses incorrect version spec: rich kid==1.0"
+    with pytest.raises(ValueError, match=match):
+        @func.register("rich kid==1.0")
+        def _(bar, baz="baz"):
+            return "bar"
